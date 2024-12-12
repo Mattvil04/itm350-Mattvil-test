06d37a15c129fcbd3415df28622c43d1e89cc14a
Mattvil04 <matisebastianvillacis@gmail.com>
2024-12-12 16:51:28 -0500

    updated

--- respaldo/bulletin-board-app/jest.config.js
+++
@@ -1,8 +0,0 @@
-// For every .js file in the backend directory, require at least 80% line coverage.
-module.exports = {
-    coverageThreshold: {
-      './backend/*.js': {
-        lines: 80
-      }
-    }
-  }
